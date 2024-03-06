07dcdc7dfecf1538fbb598f1a05bcc03ea4fd723
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-06 15:11:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function makeid(l) {
-  // write your code here
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length); // Generate random index within the range of characters
+    result += characters[randomIndex]; // Use the random index to select a character
+  }
+  
+  return result;
 }
 
+// Example usage:
+const randomString = generateRandomString(10);
+console.log(randomString); // Output will be a random string of length 10
+
+
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+const length = prompt("Enter a number.");
+ 
+alert(generateRandomString(length));
